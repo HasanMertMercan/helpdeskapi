2eebcf6eb671876df111d50033432ff459f09afb
HasanMertMercan <mercan.hasanmert@gmail.com>
2019-06-25 15:01:07 +0200

    CreateRelationBetweenTablesOnRepositories

--- src/main/java/com/mercanmert/helpdesksystem/Repositories/DeviceRepository.java
+++ src/main/java/com/mercanmert/helpdesksystem/Repositories/DeviceRepository.java
@@ -3,5 +3,8 @@ package com.mercanmert.helpdesksystem.Repositories;
 import com.mercanmert.helpdesksystem.Entities.Device;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DeviceRepository extends CrudRepository<Device, Integer> {
+    List<Device> findByUserId(int ownerId);
 }
--- src/main/java/com/mercanmert/helpdesksystem/Repositories/TicketRepository.java
+++ src/main/java/com/mercanmert/helpdesksystem/Repositories/TicketRepository.java
@@ -3,5 +3,9 @@ package com.mercanmert.helpdesksystem.Repositories;
 import com.mercanmert.helpdesksystem.Entities.Ticket;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends CrudRepository<Ticket, Integer> {
+    List<Ticket> findByUserId(int ownerId);
+    List<Ticket> findByOperatorId(int operatorId);
 }
